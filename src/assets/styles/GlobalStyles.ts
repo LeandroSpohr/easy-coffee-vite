67d1002c2ffc102fdfd74207ad959ad7ff1d3669
LeandroSpohr <leandro.spohr@acertapromotora.com.br>
2022-09-10 11:03:23 -0300

    chore: add base style

--- src/assets/styles/GlobalStyles.ts
+++ src/assets/styles/GlobalStyles.ts
@@ -1,10 +1,22 @@
 import {createGlobalStyle} from 'styled-components'
+import coffeBean from '../images/coffee_bean.svg'
+
+import { colors } from '../variables'
+
+const { brown } = colors
 
 const GlobalStyle = createGlobalStyle`
   *{
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+  }
+
+  body {
+    background-color: ${brown};
+    background-image: url(${coffeBean});
+    background-size: 600px;
+    height: 100vh;
   }
 `
 
---
+++ src/assets/variables.ts
@@ -0,0 +1,13 @@
+interface ColorsInterface {
+  darkBrown: string
+  brown: string
+  lightBrown: string
+  veryLightBrown: string
+}
+
+export const colors: ColorsInterface = {
+  darkBrown: '#240f04',
+  brown: '#5c3015',
+  lightBrown: '#9f5524',
+  veryLightBrown: '#bc906a'
+}
