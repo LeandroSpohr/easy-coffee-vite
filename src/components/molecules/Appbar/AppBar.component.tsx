31142cce9183fac9f48bab3fe022d8eb50732556
Brunoww <82423722+bruno918@users.noreply.github.com>
2022-10-29 09:18:50 -0300

    fix: fix AppBar

--- src/components/molecules/Appbar/AppBar.component.tsx
+++ src/components/molecules/Appbar/AppBar.component.tsx
@@ -4,18 +4,33 @@ import AppBar from './AppBar.style'
 import Typography from '../../atoms/Typography'
 import Button from '../../atoms/Button'
 import Cart from '../../../assets/images/cart.svg'
-import { clearUser } from '../../../context/User/actions'
-
-
+import { useUser } from '../../../context/User'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import * as UserService from '../../../services/Users'
 const AppBarComponent = () => {
-  
+  const { dispatch } = useUser()
+  const  [ cpf ] = useState<string>('')
+  const navigate = useNavigate()
+  const logout = (cpf: string) => {
+    UserService.getByCpf(cpf)
+      .then((response) => {
+        dispatch({
+          type: 'CLEAR_USER',
+        })
+      })
+      .then(() => navigate('/produtos'))
+  }
+
   return (
     <AppBar>
       <Link to="/carrinho">
         <img src={Cart} />
       </Link>
       <Typography>EASY-COFFEE</Typography>
-      <Button onClick={() => clearUser() }>LogOff</Button>
+      <Link to="/">
+        <Button onClick={() => logout(cpf)}>LogOff</Button>
+      </Link>
     </AppBar>
   )
 }
