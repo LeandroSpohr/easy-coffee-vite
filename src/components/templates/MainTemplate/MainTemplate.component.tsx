760e62ef165d8df99a122b110a4a077866a7245f
Maicon Giehl <maicongiehl@gmail.com>
2022-10-29 10:11:12 -0300

    chore: main-template

---
+++ src/components/templates/MainTemplate/MainTemplate.component.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Container from "../../atoms/Container";
+
+type MainTemplateComponentInterface = {
+    children?: JSX.Element | JSX.Element[]
+};
+
+
+const MainTemplateComponent: React.FC<MainTemplateComponentInterface>  = ({children}) => (
+    <Container 
+    >
+    {children}
+  </Container>
+)
+
+
+export default MainTemplateComponent
