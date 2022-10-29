d1dda80e45f1a05b9b41d61fd8e2091436436eab
Brunoww <82423722+bruno918@users.noreply.github.com>
2022-10-29 10:33:10 -0300

    fix: fix AppBar

--- src/components/molecules/Appbar/AppBar.component.tsx
+++ src/components/molecules/Appbar/AppBar.component.tsx
@@ -1,27 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, Navigate } from 'react-router-dom'
 import AppBar from './AppBar.style'
 import Typography from '../../atoms/Typography'
 import Button from '../../atoms/Button'
 import Cart from '../../../assets/images/cart.svg'
-import { useUser } from '../../../context/User'
-import { useState } from 'react'
+import { clearUser } from '../../../context/User/actions'
 import { useNavigate } from 'react-router-dom'
-import * as UserService from '../../../services/Users'
 const AppBarComponent = () => {
-  const { dispatch } = useUser()
-  const  [ cpf ] = useState<string>('')
   const navigate = useNavigate()
-  const logout = (cpf: string) => {
-    UserService.getByCpf(cpf)
-      .then((response) => {
-        dispatch({
-          type: 'CLEAR_USER',
-        })
-      })
-      .then(() => navigate('/produtos'))
-  }
 
+  const logout = () => {
+    clearUser()
+    navigate('/')
+  }
   return (
     <AppBar>
       <Link to="/carrinho">
@@ -29,7 +20,7 @@ const AppBarComponent = () => {
       </Link>
       <Typography>EASY-COFFEE</Typography>
       <Link to="/">
-        <Button onClick={() => logout(cpf)}>LogOff</Button>
+        <Button onClick={() => logout()}>LogOff</Button>
       </Link>
     </AppBar>
   )
