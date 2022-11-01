eb224e0b305945907cab2fdadedbc462227ee3f0
LuizGabe <luizgabrielgrupe@gmail.com>
2022-10-31 21:10:14 -0300

    Create payment interface and payment service

---
+++ src/models/interfaces/Payment.ts
@@ -0,0 +1,7 @@
+export default interface PaymentInterface {
+  id: string
+  totalValue: number
+  dateHour: string
+  createdAt: string
+  updatedAt: string
+}
---
+++ src/services/Payments/index.ts
@@ -0,0 +1,10 @@
+import api from '../../config/api'
+import PaymentInterface from '../../models/interfaces/Payment'
+const path = '/easy-coffee/v1/payment/customer/'
+
+const getAll = (UserId: string) => api.get<PaymentInterface[]>(`${path}/${UserId}/get-all`)
+  .then((response) => response.data)
+
+export {
+  getAll
+}
