d71e3f1f677824f76744929b1df9bf1b4139b0b7
LuizGabe <luizgabrielgrupe@gmail.com>
2022-11-05 10:41:29 -0300

    Using Table component with Payment API

--- src/pages/Payment/Payment.component.tsx
+++ src/pages/Payment/Payment.component.tsx
@@ -4,24 +4,41 @@ import PaymentInterface from '../../models/interfaces/Payment'
 
 import * as PaymentService from '../../services/Payments'
 
-import Typography from '../../components/atoms/Typography'
+import { useFormats } from '../../utils/useFormats'
 
-import {useUser} from '../../context/User'
+import Typography from '../../components/atoms/Typography'
+import Table from '../../components/atoms/Table'
+
+import { useUser } from '../../context/User'
 
 const Payment = () => {
-  
+  const { formatDateDDMMYYYY } = useFormats()
   const [payments, setPayments] = useState<PaymentInterface[]>([])
-  const {state} = useUser()
+  const { state } = useUser()
 
   useEffect(() => {
     PaymentService.getAll(state.user?.id)
       .then(setPayments)
   }, [])
 
+  const dados = payments.map((item) => (
+    {...item, dateHour: formatDateDDMMYYYY(item.dateHour as Date)}
+  ))
+
+  const colunas = [
+    {field: 'totalValue', header: 'Gastos'},
+    {field: 'dateHour', header: 'Data/Hora'}
+  ]
+
   return (
     <>
       <Typography>Histórico de Pagamentos</Typography>
-
+      
+      <Table 
+        data={dados} 
+        columns={colunas}
+      />
+      
     </>
   )
 }
