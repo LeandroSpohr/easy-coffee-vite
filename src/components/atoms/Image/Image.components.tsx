4182e6c00957a6a11ff2fbb0a3ffba3a75b77b3a
Maicon Giehl <maicongiehl@gmail.com>
2022-11-11 00:01:07 -0300

    refactor: Image Component

--- src/components/atoms/Image/Image.components.tsx
+++ src/components/atoms/Image/Image.components.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
+import { StyledComponentPropsWithRef } from 'styled-components'
+import Image from './Image.style'
 
-
-interface ImageInterface{
+interface ImageInterface extends StyledComponentPropsWithRef<typeof Image> {
   source?: string
 }
 
-const ImageComponent: React.FC<
-ImageInterface
-> = ({
-  source
-}: ImageInterface) => (
-  <img src={source}/>
+const ImageComponent: React.FC<ImageInterface> = ({ source, ...rest }: ImageInterface) => (
+  <Image src={source} {...rest} />
 )
 
 export default ImageComponent
