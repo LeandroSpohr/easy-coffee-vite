5fddd1f0f227f761670738418dacc7b4d3265a98
Maicon Giehl <maicongiehl@gmail.com>
2022-11-12 18:35:06 -0300

    feature: maxWidth and maxHeight properties added to ImageComponent

--- src/components/atoms/Image/Image.components.tsx
+++ src/components/atoms/Image/Image.components.tsx
@@ -4,10 +4,15 @@ import Image from './Image.style'
 
 interface ImageInterface extends StyledComponentPropsWithRef<typeof Image> {
   source?: string
+  maxWidth?: number | string
+  maxHeight?: number | string
 }
 
-const ImageComponent: React.FC<ImageInterface> = ({ source, ...rest }: ImageInterface) => (
-  <Image src={source} {...rest} />
-)
+const ImageComponent: React.FC<ImageInterface> = ({
+  source,
+  maxWidth,
+  maxHeight,
+  ...rest
+}: ImageInterface) => <Image src={source} maxWidth={maxWidth} maxHeight={maxHeight} {...rest} />
 
 export default ImageComponent
--- src/components/atoms/Image/Image.style.tsx
+++ src/components/atoms/Image/Image.style.tsx
@@ -1,5 +1,18 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+interface ImageInterface {
+  maxWidth?: number | string
+  maxHeight?: number | string
+}
 
-const Image = styled.img``
+const Image = styled.img<ImageInterface>`
+  ${({ maxWidth }: ImageInterface) =>
+    css`
+      max-width: ${maxWidth};
+    `};
+  ${({ maxHeight }: ImageInterface) =>
+    css`
+      max-height: ${maxHeight};
+    `};
+`
 
 export default Image
