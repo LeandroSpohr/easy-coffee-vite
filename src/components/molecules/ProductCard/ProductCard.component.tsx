7273069ce601f0ac463ecdbcbb3de2b55ab9cad1
Maicon Giehl <maicongiehl@gmail.com>
2022-11-12 18:55:24 -0300

    feature: new properties in ProductCardComponentInterface and local currency handling

--- src/components/molecules/ProductCard/ProductCard.component.tsx
+++ src/components/molecules/ProductCard/ProductCard.component.tsx
@@ -3,9 +3,12 @@ import Button from '../../atoms/Button'
 import Typography from '../../atoms/Typography'
 import Image from '../../atoms/Image'
 import ProductCard from './ProductCard.style'
+import { useFormats } from '../../../utils/useFormats'
 
 type ProductCardComponentInterface = {
   imgUrl: string
+  imgMaxWidth?: number | string
+  imgMaxHeight?: number | string
   title: string
   description?: string
   buttonText?: string
@@ -15,20 +18,26 @@ type ProductCardComponentInterface = {
 
 const ProductCardComponent: React.FC<ProductCardComponentInterface> = ({
   imgUrl,
+  imgMaxWidth,
+  imgMaxHeight,
   title,
   description,
   buttonText,
   price,
   handleSubmit,
-}) => (
-  <ProductCard>
-    <Image source={imgUrl} />
-    <Typography as="h2"> {title} </Typography>
-    <Typography as="p"> {description} </Typography>
-    <Typography as="p"> {buttonText} </Typography>
-    <Typography as="p"> {price} </Typography>
-    <Button onClick={() => handleSubmit()} />
-  </ProductCard>
-)
+}) => {
+  const { formatCurrency } = useFormats()
+
+  return (
+    <ProductCard>
+      <Image source={imgUrl} maxWidth={imgMaxWidth} maxHeight={imgMaxHeight} />
+      <Typography as="h2"> {title} </Typography>
+      <Typography as="p"> {description} </Typography>
+      <Typography as="p"> {buttonText} </Typography>
+      <Typography as="p"> {formatCurrency(price)} </Typography>
+      <Button onClick={() => handleSubmit()} />
+    </ProductCard>
+  )
+}
 
 export default ProductCardComponent
