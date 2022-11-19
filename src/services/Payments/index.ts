a20af583e3e8ea7df7a23bd68993828060263334
LuizGabe <luizgabrielgrupe@gmail.com>
2022-11-19 13:23:39 -0300

    Changing userId to CamelCase

--- src/services/Payments/index.ts
+++ src/services/Payments/index.ts
@@ -2,7 +2,7 @@ import api from '../../config/api'
 import PaymentInterface from '../../models/interfaces/Payment'
 const path = '/easy-coffee/v1/payment/customer/'
 
-const getAll = (UserId?: string) => api.get<PaymentInterface[]>(`${path}/${UserId}/get-all`)
+const getAll = (userId?: string) => api.get<PaymentInterface[]>(`${path}/${userId}/get-all`)
   .then((response) => response.data)
 
 export {
