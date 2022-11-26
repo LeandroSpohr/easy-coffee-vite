97bebbac348054adc7fb8e2c8e1bc27e2f93e182
LeandroSpohr <leandro.spohr@acertapromotora.com.br>
2022-11-26 01:39:31 -0300

    fix: loade size

--- src/components/atoms/Loader/Loader.component.tsx
+++ src/components/atoms/Loader/Loader.component.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Player } from '@lottiefiles/react-lottie-player'
 import coffeeLoveJson from '../../../assets/lottie/coffee-love.json'
 
+import { sizes } from '../../../assets/styles/variables'
+
+const {size300} = sizes
+
 const Loader = () => (
   <Player
     autoplay
     loop
     src={coffeeLoveJson}
-    style={{ height: '300px', width: '300px' }}
+    style={{ height: size300, width: size300 }}
   />
 )
 
