f3de616749c5b0d93b0a9fcfa186fc37562fad52
Maicon <maicongiehl@gmail.com>
2023-01-25 14:09:52 -0300

    chore: numeric input format

---
+++ src/components/atoms/NumericInputFormat/NumericInputFormat.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Container, NumericInput } from './NumericInputFormat.style'
+
+const NumericInputFormat = (props: any) => (
+  <Container>
+    <NumericInput {...props} />
+  </Container>
+)
+
+export default NumericInputFormat
